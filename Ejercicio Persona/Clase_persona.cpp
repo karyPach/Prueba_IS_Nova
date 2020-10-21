b69b68d8bd7f5779a064813b961aaa27a294374d
karyPach <73128980+karyPach@users.noreply.github.com>
2020-10-21 15:04:41 -0500

    Update Clase_persona.cpp

--- Ejercicio Persona/Clase_persona.cpp
+++ Ejercicio Persona/Clase_persona.cpp
@@ -14,3 +14,16 @@ class Persona{
 		Persona(string,int); //constructor
 		void mostrarPersona();//Mostrar nombre y edad
 };
+//Clase Alumno
+class Alumno : public Persona{
+	private: //Atributos
+		string codigoAlumno;
+		float promedio;
+	public: //Metodos
+	/*Alumno(string a, int b, string c,float d) : Persona(a,b){
+		codigoAlumno = c;
+		promedio = d;
+	}*/
+	Alumno (string,int,string,float); //Constructor
+	void mostrarAlumno();
+};
